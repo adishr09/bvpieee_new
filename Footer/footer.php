949e5019baf885e96a93dc16e3a55f1844c2a51c
kunal95 <kunsachdeva@gmail.com>
2014-08-10 23:40:36 +0530

    footer

--- Footer/footer.php
+++ Footer/footer.php
@@ -6,6 +6,7 @@
 		padding:0px;
 		}
         .col{
+		text-shadow:0px 0px 1px black;
 		width:31%;
 		float:left;
 		align-content:centre;
@@ -13,28 +14,35 @@
 		padding:1%;
 		overflow-wrap:break-word;
 		}
+		.head_f{color:#373e18;font-size:20px;}
+		#col3{text-align:right;color:white;}
     </style>
-	<div id="left_f" style="border-radius:50px 50px 0 0;background:rgba(236, 236, 236, 1);">
+	<div id="left_f" style="border-radius:50px 50px 0 0;background-image:url(footer/foot.png);color:#99cc33">
 		<div class="col" id="col1">
-            <a href="#">Link1</a><br />
-            <a href="#">Link1</a><br />
-            <a href="#">Link1</a><br />
-            <a href="#">Link1</a>
+		<span class="head_f">Get in Touch</span></br>
+            <a href="#">Join Us</a><br />
+            <a href="mailto:bvpieee@gmail.com">bvpieee@gmail.com<br />
+            <a href="#">The Team</a><br />
+            <a href="#">Feedback</a>
         </div>
         <div class="col" id="col2">
-            <a href="#">Link1</a><br />
-            <a href="#">Link1</a><br />
-            <a href="#">Link1</a><br />
-            <a href="#">Link1</a>
+           <span class="head_f">Useful Links</span></br>
+            <a href="http://www.ieee.org/">IEEE</a><br />
+            <a href="http://www.r10sac.org/">R10 Website<br />
+            <a href="http://delhi.r10sac.org/">Delhi Section</a><br />
+            <a href="www.ieee.org/membership_services/membership/my_ieee.html">myIEEE</a>
         </div>
         <div class="col" id="col3">
-            <a href="#">Link1</a><br />
-            <a href="#">Link1</a><br />
-            <a href="#">Link1</a><br />
-            <a href="#">Link1</a>
+		<span class="head_f">Reach Us</span></br>
+            <span>BVP IEEE Student Branch</span><br />
+            <span>A-4 Pashchim Vihar</span><br />
+            <span>New Delhi - 110063</span><br />
+   
         </div>
-		<p style="float:left;">BVPIEEE | Passion To Perfection</p>
-		<p style="float:right; display:inline;">Keep In Touch
-		</p>
+		<div style="margin-top:120px;height:20px;width:100%;border-top: 1px grey solid;">
+		<div style="float:left;width:50%;margin-left:-413px;">BVPIEEE | Passion To Perfection</div>
+		<div style="float:right;width:50%;display:inline;">Keep In Touch
+		</div>
+		</div>
 	</div>
 ';?>
