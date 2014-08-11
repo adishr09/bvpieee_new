e166a503e458c7bd25104934388c7ad3e9f880f1
archangel <adi.shr09@gmail.com>
2014-08-12 00:58:56 +0530

    yeah

--- Footer/footer.php
+++ Footer/footer.php
@@ -1,21 +1,32 @@
 <?php echo'
 
     <style>
-        body{
-		margin:0px;
-		padding:0px;
-		}
         .col{
-		text-shadow:0px 0px 1px black;
-		width:31%;
-		float:left;
-		align-content:centre;
-		height:auto;
-		padding:1%;
-		overflow-wrap:break-word;
+			font-weight:800;
+			width:33%;
+			float:left;
+			align-content:centre;
+			height:auto;
+			padding:1%;
+			display:inline-block;
+			overflow-wrap:break-word;
 		}
-		.head_f{color:#373e18;font-size:20px;}
-		#col3{text-align:right;color:white;}
+		.head_f{
+			color:#373e18;
+			font-size:20px;
+		}
+		.bottom{
+			position:relative;
+			border-top:3px solid black;
+			float:left;
+			width:99%;
+			padding:5px;
+		}
+		#col3{
+			text-align:right;
+			color:white;
+			display:inline-block;
+		}
     </style>
 	<div id="left_f" style="border-radius:50px 50px 0 0;background-image:url(footer/foot.png);color:#99cc33">
 		<div class="col" id="col1">
@@ -39,10 +50,9 @@
             <span>New Delhi - 110063</span><br />
    
         </div>
-		<div style="margin-top:120px;height:20px;width:100%;border-top: 1px grey solid;">
-		<div style="float:left;width:50%;margin-left:-413px;">BVPIEEE | Passion To Perfection</div>
-		<div style="float:right;width:50%;display:inline;">Keep In Touch
-		</div>
+		<div class="bottom">
+			<p style="float:left; display:inline-block;width:50%;">BVPIEEE | Passion To Perfection</p>
+			<p style="float:right; display:inline-block;">Keep In Touch</p>
 		</div>
 	</div>
 ';?>
