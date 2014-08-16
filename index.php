fc0c3818a37fa0613a412de03194d0e53c5854f9
kunal95 <kunsachdeva@gmail.com>
2014-08-17 02:02:09 +0530

    3rd box

--- index.php
+++ index.php
@@ -135,12 +135,16 @@
 		<div id="right">
 			<h3>Upcoming Events</h3></br>
 			<ul>
-			<li>Princeton Review Seminar</li>
-			<li>Equity Research </li>
+			<li>Fresh On Campus</li>
+			<li>Equity Research Initiative</li>
 			</ul>
 		</div>
 		<div id="right">
-			<p>Latest Branch News</p>
+			<h3>Latest Branch News</h3></br>
+			<ul>
+			<li><a href="http://goo.gl/DR1s1T" style="color:#8a8c7e;">New Member Registrations Open</a></li>
+			<li><a href="https://docs.google.com/forms/d/1MMbpG5zG0eWvvex9_Zl3qOoJ4ZBU97pGnBfZYK4YFhI/viewform?c=0&w=1&usp=mail_form_link" style="color:#8a8c7e;">Join BVPIEEE design team</a></li>
+			</ul>
 		</div>
 	</div>
 	<?php include ("Footer/footer.php"); ?>
