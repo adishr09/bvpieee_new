bc6160845dd9539c9ac9ccba7d21c5fe76ad80aa
kunal95 <kunsachdeva@gmail.com>
2014-09-29 14:48:00 +0530

    Update right.php

--- right.php
+++ right.php
@@ -18,8 +18,8 @@
 		<div id="right">
 			<h3 style="color:#99cc33;">Latest Branch News</h3></br>
 			<ul>
-			<li><a href="http://goo.gl/DR1s1T" style="color:#8a8c7e;">New Member Registrations Open</a></li>
-			<li><a href="https://docs.google.com/forms/d/1MMbpG5zG0eWvvex9_Zl3qOoJ4ZBU97pGnBfZYK4YFhI/viewform?c=0&w=1&usp=mail_form_link" style="color:#8a8c7e;">Join BVPIEEE design team</a></li>
+			<li><a href="http://goo.gl/DR1s1T" style="color:rgb(233,233,233);">New Member Registrations Open</a></li>
+			<li><a href="https://docs.google.com/forms/d/1MMbpG5zG0eWvvex9_Zl3qOoJ4ZBU97pGnBfZYK4YFhI/viewform?c=0&w=1&usp=mail_form_link" style="color:rgb(233,233,233);">Join BVPIEEE design team</a></li>
 			</ul>
 		</div>
 		;'?>
